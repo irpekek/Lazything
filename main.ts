7cebf553463eab1ac5b8e6bdd1803c7cbcd5917c
irpekek <sandylaksanap@gmail.com>
2024-12-04 14:35:47 +0700

    add authkey functionality, finding proxy repository, and running command

--- main.ts
+++ main.ts
@@ -11,7 +11,6 @@ interface IGhMeta {
   repository: object;
   score: number;
 }
-
 interface IGhUrl {
   sha: string;
   node_id: string;
@@ -20,12 +19,10 @@ interface IGhUrl {
   content: string;
   encoding: string;
 }
-
 interface IWsOpts {
   path: string;
   headers: string;
 }
-
 interface IProxy {
   name: string;
   type: string;
@@ -36,7 +33,6 @@ interface IProxy {
   network: string;
   'ws-opts': IWsOpts;
 }
-
 interface ITrojanProxy extends IProxy {
   password: string;
 }
@@ -44,7 +40,7 @@ interface IVmessProxy extends IProxy {
   uuid: string;
 }
 
-type ProxyType = ITrojanProxy | IVmessProxy
+type ProxyType = ITrojanProxy | IVmessProxy;
 
 function isVmess(obj: unknown): obj is IVmessProxy {
   return obj !== null && typeof obj === 'object' && 'uuid' in obj;
@@ -64,7 +60,7 @@ async function getProxies(
       { owner, repo, file_sha }
     );
     if (response.status !== 200)
-      throw new Error('Error while fetching yaml data');
+      throw new Error(`Error while fetching with status ${response.status}`);
 
     const buffer = Buffer.from(response.data.content, 'base64');
     const dataYaml = buffer.toString('utf-8');
@@ -74,36 +70,44 @@ async function getProxies(
     throw new Error('Error while fetching yaml data');
   }
 }
+async function findProxyRepo(hostname: string) {
+  const query = `${hostname} language:yaml`;
+  try {
+    const response = await octo.request('GET /search/code', { q: query });
+    if (response.status !== 200)
+      throw new Error('Error while finding repo with status' + response.status);
+    return response.data;
+  } catch (_error) {
+    throw new Error('Error while finding repo');
+  }
+}
 const hostname = 'vplay.iflix.com';
-const query = `${hostname} language:yaml`;
 
 const octo = new Octokit({
-  auth: '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',
+  auth: getAuthKey(),
 });
-
-
-const response = await octo.request('GET /search/code', {
-  q: query,
-});
-
-
-const items = response.data.items;
 const proxies: object[] = [];
 const listedPass: string[] = [];
 
-for (const item of items) {
-  const owner = item.repository.owner.login;
-  const repo = item.repository.name;
-  const sha = item.sha;
-  const proxiesResult = await getProxies(owner, repo, sha);
-  if (proxiesResult) {
-    for (const proxy of proxiesResult) {
-      if (isTrojan(proxy)) addProxy(proxy, proxy.password);
-      if (isVmess(proxy)) addProxy(proxy, proxy.uuid);
+async function runCommand() {
+  const { items } = await findProxyRepo(hostname);
+  for (const item of items) {
+    const owner = item.repository.owner.login;
+    const repo = item.repository.name;
+    const sha = item.sha;
+    const proxiesResult = await getProxies(owner, repo, sha);
+    if (proxiesResult) {
+      for (const proxy of proxiesResult) {
+        if (isTrojan(proxy)) addProxy(proxy, proxy.password);
+        if (isVmess(proxy)) addProxy(proxy, proxy.uuid);
+      }
     }
   }
+  Deno.writeTextFileSync(
+    `result ${getFullDate()}.json`,
+    JSON.stringify(proxies)
+  );
 }
-
 function addProxy(proxy: ProxyType, password: string) {
   const findPass = listedPass.find((e) => e === password);
   if (!findPass) {
@@ -111,5 +115,21 @@ function addProxy(proxy: ProxyType, password: string) {
     listedPass.push(password);
   }
 }
+function getFullDate(): string {
+  const dt = Date.now();
+  const year = new Date(dt).getFullYear();
+  const date = new Date(dt).getDate();
+  const month = new Date(dt).getMonth();
+  const hour = new Date(dt).getHours();
+  const minutes = new Date(dt).getMinutes();
+  const second = new Date(dt).getSeconds();
+  return `${date}-${month}-${year} ${hour}:${minutes}:${second}`;
+}
+function getAuthKey(): string {
+  return Deno.readTextFileSync('auth.txt');
+}
+function setAuthKey(key: string): void {
+  Deno.writeTextFileSync('auth.txt', key);
+}
 
-Deno.writeTextFile('result.json', JSON.stringify(proxies));
+runCommand();
