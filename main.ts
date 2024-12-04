202f858e755af9f1d6737a402b0848cf9939856c
irpekek <sandylaksanap@gmail.com>
2024-12-04 10:29:08 +0700

    update fetch item using restapi instead vanilla and add proxytype

--- main.ts
+++ main.ts
@@ -26,57 +26,89 @@ interface IWsOpts {
   headers: string;
 }
 
-interface IProxie {
+interface IProxy {
   name: string;
   type: string;
   server: string;
   port: number;
-  password: string;
   udp?: boolean;
   sni?: string;
   network: string;
   'ws-opts': IWsOpts;
 }
 
-async function fetchItem(url: string): Promise<IProxie[] | null> {
+interface ITrojanProxy extends IProxy {
+  password: string;
+}
+interface IVmessProxy extends IProxy {
+  uuid: string;
+}
+
+type ProxyType = ITrojanProxy | IVmessProxy
+
+function isVmess(obj: unknown): obj is IVmessProxy {
+  return obj !== null && typeof obj === 'object' && 'uuid' in obj;
+}
+function isTrojan(obj: unknown): obj is ITrojanProxy {
+  return obj !== null && typeof obj === 'object' && 'password' in obj;
+}
+
+async function getProxies(
+  owner: string,
+  repo: string,
+  file_sha: string
+): Promise<object[]> {
   try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error('Error while fetching yaml data');
-    const data = await response.json();
-    if (data != null && 'content' in data && typeof data === 'object') {
-      const buffer = Buffer.from(data.content, 'base64');
-      const dataYaml = buffer.toString('utf-8');
-      const result = YAML.parse(dataYaml);
-      return result.proxies;
-    }
-    return null;
+    const response = await octo.request(
+      'GET /repos/{owner}/{repo}/git/blobs/{file_sha}',
+      { owner, repo, file_sha }
+    );
+    if (response.status !== 200)
+      throw new Error('Error while fetching yaml data');
+
+    const buffer = Buffer.from(response.data.content, 'base64');
+    const dataYaml = buffer.toString('utf-8');
+    const result = YAML.parse(dataYaml);
+    return result.proxies;
   } catch (_error) {
     throw new Error('Error while fetching yaml data');
   }
 }
-
 const hostname = 'vplay.iflix.com';
-
 const query = `${hostname} language:yaml`;
 
 const octo = new Octokit({
   auth: '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',
 });
 
+
 const response = await octo.request('GET /search/code', {
   q: query,
 });
 
+
 const items = response.data.items;
-const proxies: IProxie[] = [];
+const proxies: object[] = [];
+const listedPass: string[] = [];
+
 for (const item of items) {
-  const proxiesResult = await fetchItem(item.git_url);
-  let prevUUID = ''
-  if(proxiesResult){
-    for (const proxie of proxiesResult) {
-      if (proxie.password !== prevUUID) proxies.push(proxie);
-      prevUUID = proxie.password
+  const owner = item.repository.owner.login;
+  const repo = item.repository.name;
+  const sha = item.sha;
+  const proxiesResult = await getProxies(owner, repo, sha);
+  if (proxiesResult) {
+    for (const proxy of proxiesResult) {
+      if (isTrojan(proxy)) addProxy(proxy, proxy.password);
+      if (isVmess(proxy)) addProxy(proxy, proxy.uuid);
     }
+  }
+}
+
+function addProxy(proxy: ProxyType, password: string) {
+  const findPass = listedPass.find((e) => e === password);
+  if (!findPass) {
+    proxies.push(proxy);
+    listedPass.push(password);
   }
 }
 
